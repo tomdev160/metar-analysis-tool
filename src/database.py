8066033e6d9df2d009440c5481aca453c34590ad
tomdev160 <tomdevaal@outlook.com>
2026-02-17 10:39:02 +0100

    Added method get_non_compliant_days_by_limit to group non-compliant days by limit category

--- src/database.py
+++ src/database.py
@@ -1,15 +1,14 @@
 def get_non_compliant_days_by_limit(self):
-    non_compliant_days = {}
+        """
+        Returns non-compliant days grouped by limit category.
+        """
+        non_compliant_days = self.get_non_compliant_days()
+        grouped_days = {}
 
-    # Assuming 'data' is a list of records with 'date' and 'limit' fields
-    for record in self.data:
-        date = record['date']
-        limit = record['limit']
+        for day in non_compliant_days:
+            limit_category = day['limit_category']  # Assumed the structure of the data
+            if limit_category not in grouped_days:
+                grouped_days[limit_category] = []
+            grouped_days[limit_category].append(day)
 
-        # Check if the record is non-compliant
-        if not self.is_compliant(record):
-            if limit not in non_compliant_days:
-                non_compliant_days[limit] = []
-            non_compliant_days[limit].append(date)
-
-    return non_compliant_days
+        return grouped_days
